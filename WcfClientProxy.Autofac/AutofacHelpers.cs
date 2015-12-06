c606015edcca76facb8f7458cce7b3099a31580f
mihalag <mihalag86@gmail.com>
2015-12-06 21:27:20 +0300

    .

---
+++ WcfClientProxy.Autofac/AutofacHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace WcfClientProxy.Autofac
+{
+    public static class AutofacHelpers
+    {
+        public static ContainerBuilder RegisterWcfProxy<T>(this ContainerBuilder containerBuilder, IChannelFactoryProvider<T> channelFactoryProvider)  where T : class
+        {
+            containerBuilder.Register(c => new ClientProxyGenerator().CreateProxy<T>(channelFactoryProvider)).As<T>();
+            return containerBuilder;
+        }
+    }
+}
